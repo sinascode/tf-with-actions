3412d4b860b855c777e927cc2b6b4049677b48e5
sinascode <sina@dankook.ac.kr>
2025-06-25 13:12:12 +0900

    fix *.tf

--- modules/dynamodb/outputs.tf
+++ modules/dynamodb/outputs.tf
@@ -1,4 +1,4 @@
 output "dynamodb_table_name" {
-    description = "DynamoDB table name"
-    value = aws_dynamodb_table.terraform-backend-lock.name
+  description = "DynamoDB table name"
+  value       = aws_dynamodb_table.terraform-backend-lock.name
 }
--- provider.tf
+++ provider.tf
@@ -7,11 +7,11 @@ terraform {
   }
   # 같은 리소스?면 output으로 참조하면됨
   backend "s3" {
-	  bucket         = "backend-bucket-ce00"   
-	  key            = "terraform/state-test/terraform.tfstate"  # tfstate가 이곳에 저장됨
-	  region         = "ap-northeast-2"
-      dynamodb_table = "terraform-lock"  #dynamodb에서 만든 테이블 이름
-	}
+    bucket         = "backend-bucket-ce00"
+    key            = "terraform/state-test/terraform.tfstate" # tfstate가 이곳에 저장됨
+    region         = "ap-northeast-2"
+    dynamodb_table = "terraform-lock" #dynamodb에서 만든 테이블 이름
+  }
 }
 
 provider "aws" {
